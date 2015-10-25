6ec7ac9c7b53a8e8b5223d32aa61de879683edc8
Nick Zhang <thesadboy@qq.com>
2015-10-25 16:24:09 +0800

    +集合

---
+++ node+pptpd+shadowsocks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "download and install pptpd.sh"
+wget https://raw.githubusercontent.com/thesadboy/CentOS/master/pptpd.sh
+sh ./pptpd.sh nickzhang zhangliangnick
+echo "download and install node-install.sh"
+wget https://raw.githubusercontent.com/thesadboy/CentOS/master/node-install.sh
+sh ./node-install.sh
+echo "download shadowsocks.sh"
+wget https://raw.githubusercontent.com/thesadboy/CentOS/master/shadowsocks.sh
+sh ./shadowsocks.sh
